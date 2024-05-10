b9f1e9dab0334d68ea2b44063827669b8ef94a08
alexxxQA <165876230+alexxxQA@users.noreply.github.com>
2024-05-10 17:20:18 +0300

    Add files via upload

---
+++ SeleniumTest/main.py
@@ -0,0 +1,31 @@
+import requests
+
+URL = 'https://api.pokemonbattle.me/v2'
+TOKEN = '764661ce0c8df504d22e827d41ecf450'
+HEADER = {'Content-Type':'application/json', 'trainer_token':TOKEN}
+body_create = {
+    "name": "Tom",
+    "photo": "https://dolnikov.ru/pokemons/albums/008.png"
+}
+body_changename = {
+    "pokemon_id": "id_pokemon",
+    "name": "New Name",
+    "photo": "https://dolnikov.ru/pokemons/albums/008.png"
+}
+body_addpokeball = {
+    "pokemon_id": "26825"
+
+}
+
+
+'''response_create = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_create)
+print(response_create.text)'''
+
+'''message = response_create.json()['message']
+print(message)'''
+
+"""response_changename = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_changename)
+print(response_changename.text)"""
+
+response_addpokeball = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_addpokeball)
+print(response_addpokeball.text)
---
+++ SeleniumTest/tests/test_pokemon.py
@@ -0,0 +1,29 @@
+import requests
+import pytest
+
+URL = 'https://api.pokemonbattle.me/v2'
+TOKEN = '764661ce0c8df504d22e827d41ecf450'
+HEADER = {'Content-Type':'application/json', 'trainer_token':TOKEN}
+TRAINER_ID = 2267
+
+'''def test_status_code():
+    response = requests.get(url = f'{URL}/pokemons', params={'trainer_id':TRAINER_ID})
+    assert response.status_code == 200'''
+
+'''def test_part_of_response():
+    response_get = requests.get(url = f'{URL}/pokemons', params={'trainer_id':TRAINER_ID})
+    assert response_get.json()["data"][0]["name"] == 'New Name'
+'''
+def test_status_code():
+    response = requests.get(url = f'{URL}/trainers', params={'trainer_id':TRAINER_ID})
+    assert response.status_code == 200   
+    
+def test_part_of_response():
+    response_get = requests.get(url = f'{URL}/trainers', params={'trainer_id':TRAINER_ID})
+    assert response_get.json()["data"][0]["id"] == '2267'
+
+
+@pytest.mark.parametrize('key, value', [('id', '2267'), ('trainer_name', 'Clown'), ('city', 'Tokyo')])
+def test_parametrize(key, value):
+    response_parametrize = requests.get(url = f'{URL}/trainers', params={'trainer_id':TRAINER_ID})
+    assert response_parametrize.json()["data"][0][key] == value
